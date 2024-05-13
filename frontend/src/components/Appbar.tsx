201075c3dd0c10fef594167501947f046a980d63
kunal Walmiki <kunalwalmiki15@gmail.com>
2024-05-13 13:47:32 +0530

    Change Made to UI Components

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,10 +1,12 @@
 import { Avatar, Button } from '@chakra-ui/react';
 import { Link, useNavigate } from 'react-router-dom'
+import { useToast } from "@chakra-ui/react"
 
 const Appbar = () => {
 
   const navigate = useNavigate();
-  
+  const toast = useToast();
+
   return (
     <div className='bg-slate-200 w-screen h-[50px] flex items-center'>
         <div className='flex items-center w-10/12 mx-auto justify-between'>
@@ -21,6 +23,13 @@ const Appbar = () => {
                   localStorage.getItem("token") && <Button
                   onClick={()=> {
                     localStorage.removeItem("token");
+                    toast({
+                      title: `Logged Out`,
+                      position : "top",
+                      status: `success`,
+                      duration: 9000,
+                      isClosable: true,
+                    })
                     navigate("/");
                   }}
                   colorScheme='teal' variant='outline'>
